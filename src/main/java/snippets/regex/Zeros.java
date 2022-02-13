14a61e5c3985c4193b2202c7f7eda648750b6125
Debo <mail@deboder.nl>
2022-02-13 09:56:16 +0100

    zeros

--- src/main/java/snippets/regex/Zeros.java
+++ src/main/java/snippets/regex/Zeros.java
@@ -1,2 +1,13 @@
-package snippets.regex;public class Zeros {
+package snippets.regex;
+
+public class Zeros {
+
+
+    /**
+     * removes leading zeros unless it's the last character from a string.
+     */
+    public static String stripLeadingZeros(String string) {
+        if (string == null) return null;
+        return string.replaceAll("^0+(?!$)", "");
+    }
 }
--- src/test/java/snippets/regex/ZerosTest.java
+++ src/test/java/snippets/regex/ZerosTest.java
@@ -1,2 +1,32 @@
-package snippets.regex;public class ZerosTEst {
+package snippets.regex;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ZerosTest {
+
+    @Test
+    void leadingZerosRemoved1() {
+        String str = "00a";
+        assertEquals("a", Zeros.stripLeadingZeros(str));
+    }
+
+    @Test
+    void leadingZerosRemoved2() {
+        String str = "001";
+        assertEquals("1", Zeros.stripLeadingZeros(str));
+    }
+
+    @Test
+    void leadingZerosRemovedMiddleUnaffected() {
+        String str = "001001";
+        assertEquals("1001", Zeros.stripLeadingZeros(str));
+    }
+
+    @Test
+    void leadingZerosRemovedLastRemains() {
+        String str = "0000";
+        assertEquals("0", Zeros.stripLeadingZeros(str));
+    }
 }
